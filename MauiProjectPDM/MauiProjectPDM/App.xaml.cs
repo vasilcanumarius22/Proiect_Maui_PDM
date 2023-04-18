f312b1c1d58444aca6fcfb7b7c2d59a16e236670
Marius Daniel Vasilcanu <74616980+MariusVasilcanu28@users.noreply.github.com>
2023-04-18 18:52:44 +0300

    first program

---
+++ MauiProjectPDM/MauiProjectPDM/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiProjectPDM;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiProjectPDM/MauiProjectPDM/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiProjectPDM;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
