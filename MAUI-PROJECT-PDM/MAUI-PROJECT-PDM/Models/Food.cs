37e413354ee53a342ea9fc845b8da8d019dd4288
Paducel Alexandru <paducelalexandru@gmail.com>
2023-04-18 20:47:37 +0300

    food attributes

--- MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Models/Food.cs
+++ MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Models/Food.cs
@@ -2,13 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MAUI_PROJECT_PDM.Models
 {
+    [SQLite.Table("foods")]
     public class Food
     {
-        public 
-       
+        [PrimaryKey, AutoIncrement]
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        private string title;
+
+        [JsonPropertyName("type")]
+        private string type;
+
+        [JsonPropertyName("description")]
+        private string description;
+
+        [JsonPropertyName("calories")]
+        private float calories;
+
+        [JsonPropertyName("urlImage")]
+        private string urlImage;
+
+        public Food(string title, string type, string description, float calories, string urlImage)
+        {
+            this.title = title;
+            this.type = type;
+            this.description = description;
+            this.calories = calories;
+            this.urlImage = urlImage;
+        }
     }
 }
