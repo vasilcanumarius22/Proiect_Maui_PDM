13f834d6159a6d65cbd22ae5e8114f3491a69bdc
Marius Daniel Vasilcanu <114442713+vasilcanumarius22@users.noreply.github.com>
2023-04-19 18:46:28 +0300

    delete

--- MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MAUI_PROJECT_PDM;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MAUI_PROJECT_PDM;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
