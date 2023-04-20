1e15cb30c960409628f2765e8b4a9220bdd90b2b
Paducel Alexandru <paducelalexandru@gmail.com>
2023-04-20 20:11:23 +0300

    reading from json

--- MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Models/Food.cs
+++ MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Models/Food.cs
@@ -16,42 +16,30 @@ namespace MAUI_PROJECT_PDM.Models
         [JsonIgnore]
         public int Id { get; set; }
 
-        [JsonPropertyName("title")]
-        private string title;
-
-        [JsonPropertyName("type")]
-        private string type;
-
-        [JsonPropertyName("description")]
-        private string description;
-
-        [JsonPropertyName("calories")]
-        private float calories;
-
-        [JsonPropertyName("urlImage")]
-        private string urlImage;
-
         public Food(string title, string type, string description, float calories, string urlImage)
         {
-            this.title = title;
-            this.type = type;
-            this.description = description;
-            this.calories = calories;
-            this.urlImage = urlImage;
+            Title = title;
+            Type = type;
+            Description = description;
+            Calories = calories;
+            UrlImage = urlImage;
         }
 
-        public string Title { get { return title; } set { title = value; } }
-        public string Type { get { return type; } set { type = value; } }
-        public string Description
-        {
-            get { return description; }
-            set
-            {
-                description = value;
-            }
-        }
-        public float Calories { get { return calories; } set { calories = value; } }
-        public string UrlImage { get { return urlImage; } }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description {get; set;}
+
+        [JsonPropertyName("calories")]
+        public float Calories { get; set; }
+
+        [JsonPropertyName("urlImage")]
+        public string UrlImage { get; set; }
 
 
 
--- MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Views/MainPage.xaml.cs
+++ MAUI-PROJECT-PDM/MAUI-PROJECT-PDM/Views/MainPage.xaml.cs
@@ -1,22 +1,36 @@
 ﻿using MAUI_PROJECT_PDM.Models;
 using MAUI_PROJECT_PDM.Views;
+using System.Net.Http.Json;
 using System.Windows.Input;
 
 namespace MAUI_PROJECT_PDM;
 
 public partial class MainPage : ContentPage
 {
+    private const string URL_FOODS = "https://raw.githubusercontent.com/Alexsandrux/Json-File-Examples/main/foods-json.json";
     private User user;
-	public MainPage(User userDB)
+    private List<Food> foods = new List<Food>();
+    public MainPage(User userDB)
 	{
 		InitializeComponent();
         user = userDB;
-        listViewFoods.ItemsSource = MockFoods.foods;
-	}
-    // TODO: De infrumusetat cum arata un element din lista
-    // TODO: De citit elemente din JSON + de realizat json-ul
 
-    // TODO: De adaugat restul paginilor
+        getFoods();
+
+
+	}
+
+    private async void getFoods()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+
+            this.foods = await client.GetFromJsonAsync<List<Food>>(URL_FOODS);
+
+            listViewFoods.ItemsSource = this.foods;
+
+        }
+    }
     private void Toolbar_Profile_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new ProfilePage(user));
@@ -30,7 +44,6 @@ public partial class MainPage : ContentPage
 
     public void OpenFoodDetail(object sender, EventArgs e)
     {
-        //Handle the button click logic here
         Navigation.PushAsync(new FoodDetail(listViewFoods.SelectedItem as Food));
     }
 
